a2bda8ccaa01ef8cdbe4fe26cafefb40654c9e7d
wydkidwydkid4345 <95831979+wydkidwydkid4345@users.noreply.github.com>
2021-12-09 22:41:54 -0500

    xd

--- xdxdxdxdxdxdxdxdxdxdxdxdxdxdxd.lua
+++ xdxdxdxdxdxdxdxdxdxdxdxdxdxdxd.lua
@@ -1,5 +1,3 @@
--- // Aimlock
-
 getgenv().OldAimPart = "UpperTorso"
 getgenv().AimPart = "UpperTorso" 
 getgenv().AimlockKey = "q"
@@ -209,15 +207,118 @@ if getgenv().AutoPrediction == true then
             end
 end
 
--- // Esp Module
+IsFirstPerson = false
+	WHeld = false
+	SHeld = false
+	AHeld = false
+	DHeld = false
+	local L_167_ = true
+    local activar = false
+	urspeed = 0.1
+	function ChangeFaster(L_168_arg0, L_169_arg1)
+		if L_168_arg0.KeyCode == Enum.KeyCode.Down and L_169_arg1 == false then
+			urspeed = urspeed - 0.01
+		end
+	end
+	function ChangeSlower(L_170_arg0, L_171_arg1)
+		if L_170_arg0.KeyCode == Enum.KeyCode.Up and L_171_arg1 == false then
+			urspeed = urspeed + 0.01
+		end
+	end
+	function GChecker(L_172_arg0, L_173_arg1)
+		if L_172_arg0.KeyCode == Enum.KeyCode.N and L_173_arg1 == false then
+			if L_167_ == false then
+				L_167_ = true
+			elseif L_167_ == true then
+				L_167_ = false
+			end
+		end
+	end
+	function PressW(L_178_arg0, L_179_arg1)
+		if L_178_arg0.KeyCode == Enum.KeyCode.W and L_179_arg1 == false and L_167_ == true then
+			WHeld = true
+		end
+	end
+	function ReleaseW(L_180_arg0, L_181_arg1)
+		if L_180_arg0.KeyCode == Enum.KeyCode.W then
+			WHeld = false
+		end
+	end
+	function PressS(L_182_arg0, L_183_arg1)
+		if L_182_arg0.KeyCode == Enum.KeyCode.S and L_183_arg1 == false and L_167_ == true then
+			SHeld = true
+		end
+	end
+	function ReleaseS(L_184_arg0, L_185_arg1)
+		if L_184_arg0.KeyCode == Enum.KeyCode.S then
+			SHeld = false
+		end
+	end
+	function PressA(L_186_arg0, L_187_arg1)
+		if L_186_arg0.KeyCode == Enum.KeyCode.A and L_187_arg1 == false and L_167_ == true then
+			AHeld = true
+		end
+	end
+	function ReleaseA(L_188_arg0, L_189_arg1)
+		if L_188_arg0.KeyCode == Enum.KeyCode.A then
+			AHeld = false
+		end
+	end
+	function PressD(L_190_arg0, L_191_arg1)
+		if L_190_arg0.KeyCode == Enum.KeyCode.D and L_191_arg1 == false and L_167_ == true then
+			DHeld = true
+		end
+	end
+	function ReleaseD(L_192_arg0, L_193_arg1)
+		if L_192_arg0.KeyCode == Enum.KeyCode.D then
+			DHeld = false
+		end
+	end
+	function CheckFirst(L_194_arg0, L_195_arg1)
+		if L_194_arg0.KeyCode == Enum.UserInputType.MouseWheel then
+			if (player.Character.Head.CFrame.p - workspace.CurrentCamera.CFrame.p).magnitude < 0.6 then
+				IsFirstPerson = true
+			elseif (player.Character.Head.CFrame.p - workspace.CurrentCamera.CFrame.p).magnitude > 0.6 then
+				IsFirstPerson = false
+			end
+		end
+	end
+	game:GetService("UserInputService").InputBegan:connect(PressW)
+	game:GetService("UserInputService").InputEnded:connect(ReleaseW)
+	game:GetService("UserInputService").InputBegan:connect(PressS)
+	game:GetService("UserInputService").InputEnded:connect(ReleaseS)
+	game:GetService("UserInputService").InputBegan:connect(PressA)
+	game:GetService("UserInputService").InputEnded:connect(ReleaseA)
+	game:GetService("UserInputService").InputBegan:connect(PressD)
+	game:GetService("UserInputService").InputEnded:connect(ReleaseD)
+	game:GetService("UserInputService").InputChanged:connect(CheckFirst)
+	game:GetService("UserInputService").InputBegan:connect(ChangeFaster)
+	game:GetService("UserInputService").InputBegan:connect(ChangeSlower)
+	game:GetService("RunService").Stepped:connect(
+            function()
+		if activar == true then
+			if WHeld == true then
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =
+                            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0, -urspeed)
+			end
+			if SHeld == true then
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =
+                            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0, urspeed)
+			end
+			if DHeld == true then
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =
+                            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(urspeed, 0, 0)
+			end
+			if AHeld == true then
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =
+                            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(-urspeed, 0, 0)
+			end
+		end
+end)
 
 local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/wydkidwydkid4345/noonecarethisbrololwydxdxd/main/espmodule.lua", true))()
 
--- // Silent Aim
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/wydkidwydkid4345/noonecarethisbrololwydxdxd/main/base.lua", true))()
-
--- // UI
 
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/wydkidwydkid4345/noonecarethisbrololwydxdxd/main/uilib.lua"))()
 local window = library:CreateWindow("privateware-rewrite", Vector2.new(492, 598), Enum.KeyCode.V)
@@ -227,70 +328,80 @@ local B_2_ = window:CreateTab("Misc")
 local B_3_ = window:CreateTab("Visual")
 local B_4_ = window:CreateTab("Teleports")
 
-local L_1_ = B_1_:CreateSector("Aimbot", "left")
-local L_2_ = B_1_:CreateSector("Aimbot; Settings", "right")
-local L_3_ = B_1_:CreateSector("Silent Aim", "left")
-local L_4_ = B_1_:CreateSector("Silent Aim; Settings", "right")
-local L_5_ = B_1_:CreateSector("Esp", "left")
-local L_6_ = B_1_:CreateSector("Settings", "right")
+local section1 = B_1_:CreateSector("Aimbot", "left")
+local section2 = B_1_:CreateSector("Aimbot; Settings", "right")
+local section3 = B_1_:CreateSector("Silent Aim", "left")
+local section4 = B_1_:CreateSector("Silent Aim; Settings", "right")
+local section5 = B_1_:CreateSector("Esp", "left")
+local section6 = B_1_:CreateSector("Settings", "right")
+local section7 = B_2_:CreateSector("Animations", "left")
+local section8 = B_2_:CreateSector("CFrame Speed", "right")
 
-L_1_:AddToggle("Enabled", false, function(bro1)
-	Aimlock = bro1
+section1:AddToggle("Enabled", false, function(alr1)
+    Aimlock = alr1
 end):AddKeybind("None")
 
-L_1_:AddToggle("Ping Based Prediction", false, function(bro2)
-    getgenv().AutoPrediction = bro2
+section1:AddToggle("Ping Based Prediction", false, function(alr2)
+    getgenv().AutoPrediction = alr2
 end)
 
-L_1_:AddToggle("Airshot Function", false, function(bro3)
-    getgenv().CheckIfJumped = bro3
+section1:AddToggle("Airshot Function", false, function(alr3)
+    getgenv().CheckIfJumped = alr3
 end)
 
-L_1_:AddDropdown("Hitbox", {"Head", "UpperTorso", "HumanoidRootPart", "LowerTorso", "LeftUpperLeg", "RightUpperLeg", "LeftLowerLeg", "RightLowerLeg", "LeftFoot", "RightFoot"}, "HumanoidRootPart", false, function(bro4)
-    getgenv().AimPart = bro4
+section1:AddToggle("Team Check", false, function(alr4)
+    getgenv().TeamCheck = alr4
 end)
 
-L_1_:AddTextbox("Aimlock Prediction", "", function(bro5)
-    getgenv().PredictionVelocity = bro5
+section1:AddDropdown("Hitbox", {"Head", "UpperTorso", "HumanoidRootPart", "LowerTorso", "LeftUpperLeg", "RightUpperLeg", "LeftLowerLeg", "RightLowerLeg", "LeftFoot", "RightFoot"}, default, false, function(alr5)
+    getgenv().AimPart = alr5
 end)
 
-L_2_:AddTextbox("Aimlock Key", "q", function(bro6)
-    getgenv().AimlockKey = bro6
+section1:AddTextbox("Bullet Prediction", "", function(lockpredictionlolxdd)
+    getgenv().PredictionVelocity = lockpredictionlolxdd
 end)
 
-L_2_:AddSlider("Aim Radius", 0, 1, 30, decimals, function(bro7)
-    getgenv().AimRadius = bro7
+section2:AddTextbox("Aimlock Key", "q", function(bindasd)
+    getgenv().AimlockKey = bindasd
+  end)
+
+section2:AddSlider("Aim Radius", 1, 30, 100, decimals, function(alr21)
+    getgenv().AimRadius = alr21
 end)
 
-L_3_:AddToggle("Enabled", false, function(bro8)
-    DaHoodSettings.SilentAim = bro8
+section3:AddToggle("Enabled", false, function(alr10)
+    DaHoodSettings.SilentAim = alr10
 end):AddKeybind("None")
 
-L_3_:AddToggle("Visible Check", false, function(bro9)
-    Aiming.VisibleCheck = bro9
+section3:AddToggle("Visible Check", false, function(alr12)
+    Aiming.VisibleCheck = alr12
 end)
 
-L_3_:AddToggle("K0d Check", false, function(bro10)
-    Aiming.Check().K0d = bro10
+section3:AddToggle("K0d Check", false, function(alr13)
+    Aiming.Check().K0d = alr13
 end)
 
-L_3_:AddToggle("Grabbed Check", false, function(bro11)
-    Aiming.Check().Grabbed = bro11
+section3:AddToggle("Grabbed Check", false, function(alr14)
+    Aiming.Check().Grabbed = alr14
 end)
 
-L_3_:AddTextbox("Bullet Prediction", "", function(prediction)
-    DaHoodSettings.Prediction = prediction
+section3:AddDropdown("Hitbox", {"Head", "UpperTorso", "HumanoidRootPart", "LowerTorso", "LeftUpperLeg", "RightUpperLeg", "LeftLowerLeg", "RightLowerLeg", "LeftFoot", "RightFoot"}, default, false, function(silenthitbox)
+    Aiming.TargetPart = silenthitbox
 end)
 
-L_4_:AddToggle("Show Fov", false, function(bro12)
-    Aiming.ShowFOV = bro12
+section3:AddTextbox("Bullet Prediction", "", function(silentpredictionlolxdd)
+    DaHoodSettings.Prediction = silentpredictionlolxdd
+end)
+
+section4:AddToggle("Show Fov", false, function(alr15)
+    Aiming.ShowFOV = alr15
 end):AddKeybind("None")
 
-L_4_:AddToggle("Filled", false, function(bro13)
-    Aiming.Filled = bro13
+section4:AddToggle("Filled", false, function(alr16)
+    Aiming.Filled = alr16
 end)
 
-L_4_:AddDropdown("Shape", {"Custom", "Circle", "Square"}, "Custom", false, function(v)
+section4:AddDropdown("Shape", {"Custom", "Circle", "Square"}, "Custom", false, function(v)
     if v == "Custom" then
         Aiming.FOV = 30
         Aiming.FOVSides = 100
@@ -306,45 +417,149 @@ L_4_:AddDropdown("Shape", {"Custom", "Circle", "Square"}, "Custom", false, funct
     end
 end)
 
-L_4_:AddSlider("Size", 1, 30, 300, decimals, function(bro14)
+section4:AddSlider("Size", 1, 30, 300, decimals, function(bro14)
     Aiming.FOV = bro14
 end)
 
-L_4_:AddSlider("Round", 1, 40, 40, decimals, function(bro15)
+section4:AddSlider("Round", 1, 40, 40, decimals, function(bro15)
     Aiming.FOVSides = bro15
 end)
 
-L_4_:AddSlider("Transparency", 0, 0.5, 1, 10, function(bro16)
+section4:AddSlider("Transparency", 0, 0.5, 1, 10, function(bro16)
     Aiming.Transparency = bro16
 end)
 
-L_5_:AddToggle("Esp Toggle", false, function(bro17)
+section5:AddToggle("Esp Toggle", false, function(bro17)
     ESP:Toggle(bro17)
 end):AddKeybind("None")
 
-L_5_:AddToggle("Boxes", false, function(bro18)
+section5:AddToggle("Boxes", false, function(bro18)
     ESP.Boxes = bro18
 end)
 
-L_5_:AddToggle("Names", false, function(bro19)
+section5:AddToggle("Names", false, function(bro19)
     ESP.Names = bro19
 end)
 
-L_5_:AddToggle("Tracers", false, function(bro20)
+section5:AddToggle("Tracers", false, function(bro20)
     ESP.Tracers = bro20
 end)
 
-L_5_:AddSlider("Attach Shift", 0, 1, 10, decimals, function(bro21)
+section5:AddSlider("Attach Shift", 0, 1, 10, decimals, function(bro21)
     ESP.AttachShift = bro21
 end)
 
-local whitelist = L_6_:AddTextbox("Player Username", "", function()
+local whitelist = section6:AddTextbox("Player Username", "", function()
 end)
 
-L_6_:AddButton("Add Whitelist", function()
+section6:AddButton("Add Whitelist", function()
     Aiming.IgnorePlayer(whitelist)
 end)
 
-L_6_:AddButton("Remove Whitelist", function()
+section6:AddButton("Remove Whitelist", function()
     Aiming.UnIgnorePlayer(whitelist)
+end)
+
+section7:AddButton("Zombie and Werewolf-Mage", function()
+	local Animate = game.Players.LocalPlayer.Character.Animate
+	Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=616158929"
+	Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=616160636"
+	Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
+	Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
+	Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=1083218792"
+	Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=707829716"
+end)
+
+section7:AddButton("Zombie and Rthro-OldSchool", function()
+	local Animate = game.Players.LocalPlayer.Character.Animate
+	Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=616158929"
+	Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=616160636"
+	Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
+	Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
+	Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=2510197830"
+	Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=5319839762"
+end)
+
+section7:AddButton("Zombie and Ninja", function()
+	local Animate = game.Players.LocalPlayer.Character.Animate
+	Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=782841498"
+	Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=782845736"
+	Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
+	Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
+	Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=656117878"
+	Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=656115606"
+end)
+
+section7:AddButton("Zombie and Knight", function()
+	local Animate = game.Players.LocalPlayer.Character.Animate
+	Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=657595757"
+	Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=657568135"
+	Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
+	Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
+	Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=658409194"
+	Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=657600338"
+end)
+
+section7:AddButton("Ninja and Rthro", function()
+	local Animate = game.Players.LocalPlayer.Character.Animate
+	Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=656117400"
+	Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=656118341"
+	Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=656117878"
+	Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=656115606"
+end)
+
+section7:AddButton("Zombie and Ninja-Mage", function()
+	local Animate = game.Players.LocalPlayer.Character.Animate
+	Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=782841498"
+	Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=782845736"
+	Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
+	Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
+	Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=656117878"
+	Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=707829716"
+	Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=616003713"
+end)
+
+section7:AddButton("Keo's Animation", function()
+	local Animate = game.Players.LocalPlayer.Character.Animate
+	Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=2510196951"
+	Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=2510197257"
+	Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
+	Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
+	Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=1083218792"
+	Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=707829716"
+	Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=616003713"
+end)
+
+
+section8:AddToggle("CFrame Speed", false, function(cframe)
+    activar = cframe
+end):AddKeybind("None")
+
+section8:AddButton("Fix CFrame", function(cframe)
+    for _, v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+        if v:IsA("Script") and v.Name ~= "Health" and v.Name ~= "Sound" and v:FindFirstChild("LocalScript") then
+            v:Destroy()
+        end
+    end
+    game.Players.LocalPlayer.CharacterAdded:Connect(function(char)
+        repeat
+            wait()
+        until game.Players.LocalPlayer.Character
+        char.ChildAdded:Connect(function(child)
+            if child:IsA("Script") then 
+                wait(0.1)
+                if child:FindFirstChild("LocalScript") then
+                    child.LocalScript:FireServer()
+                end
+            end
+        end)
+    end)
+
+end)
+
+local glitch = false
+local clicker = false
+
+section8:AddSlider("Speed", -10, 0, 10, 5, function(ass)
+    urspeed = ass
 end)
